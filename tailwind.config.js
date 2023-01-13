9b2efa6e8b3a8bab47853af573850977fbcfe3c3
Halim-Shams <halimshams8@gmail.com>
2023-01-13 09:26:16 +0430

    design added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {},
+        fontFamily: {
+            custom: ['Press Start 2P', 'cursive'],
+        },
+    },
+    plugins: [],
+};
